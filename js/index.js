42d4f08cd9a33251160487ff9156f6702c872834
HarleyMartins <harleyfla05@gmail.com>
2024-07-09 08:57:06 -0300

    feat: objeto "atividade" e function de criarAtividade adicionados

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// object
+const atividade = {
+  nome: "Almoço",
+  data: new Date("2024-07-08 10:00"),
+  finalizada: true,
+}
+
+// arrow function
+const criarItemDeAtividade = (atividade) => {
+  let input = `<input type="checkbox" `
+
+  if (atividade.finalizada) {
+    input = input + "checked"
+  }
+
+  input = input + ">"
+
+  return `
+  <div>
+    ${input}
+    <span>${atividade.nome}</span>
+    <time>${atividade.data}</time>
+  </div>
+  `
+}
+
+const section = document.querySelector("section")
+section.innerHTML = criarItemDeAtividade(atividade)
