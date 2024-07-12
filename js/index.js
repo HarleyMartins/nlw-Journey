4b085ba6b87c076f2b9986b0ebe528ea970e6710
HarleyMartins <harleyfla05@gmail.com>
2024-07-12 17:19:18 -0300

    feat: lógica de salvar a atividade criada

--- js/index.js
+++ js/index.js
@@ -62,6 +62,7 @@ const criarItemDeAtividade = (atividade) => {
 
 const atualizarLista = () => {
   const section = document.querySelector("section")
+  section.innerHTML = ""
   // verifica se a lista está vazia
   if (atividades.length == 0) {
     section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`
@@ -74,3 +75,66 @@ const atualizarLista = () => {
 }
 
 atualizarLista()
+
+const salvarAtividade = (event) => {
+  event.preventDefault()
+  const dadosDoFormulario = new FormData(event.target)
+
+  const nome = dadosDoFormulario.get("atividade")
+  const dia = dadosDoFormulario.get("dia")
+  const hora = dadosDoFormulario.get("hora")
+  const data = `${dia} ${hora}`
+
+  const novaAtividade = {
+    nome,
+    data,
+    finalizada: false,
+  }
+
+  const atividadeExiste = atividades.find((atividade) => {
+    return atividade.data == novaAtividade.data
+  })
+
+  if (atividadeExiste) {
+    return alert("Dia/Hora não disponível")
+  }
+
+  atividades = [novaAtividade, ...atividades]
+  atualizarLista()
+}
+
+const criarDiasSelecao = () => {
+  const dias = [
+    "2024-02-29",
+    "2024-02-28",
+    "2024-03-01",
+    "2024-03-02",
+    "2024-03-03",
+  ]
+
+  let diasSelecao = ""
+
+  for (let dia of dias) {
+    const formatar = formatador(dia)
+    const diaFormatado = `${formatar.dia.numerico} de ${formatar.mes}`
+    diasSelecao += ` <option value="${dia}">${diaFormatado}</option> `
+  }
+
+  document.querySelector("select[name='dia']").innerHTML = diasSelecao
+}
+criarDiasSelecao()
+
+const criarHorasSelecao = () => {
+  const horas = ["5:30", "6:00", "6:30", "7:00", "7:30", "8:00"]
+
+  let horasDisponiveis = ""
+
+  for (let i = 6; i < 23; i++) {
+    horasDisponiveis += ` <option value="${i}:00">${i}:00</option> `
+    horasDisponiveis += ` <option value="${i}:15">${i}:15</option> `
+  }
+
+  document.querySelector("select[name='hora']").innerHTML = horasDisponiveis
+}
+
+criarHorasSelecao()
